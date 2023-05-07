6493e7cabcb4fba4a90d20e9540e3aa2f9270998
Dean Daryl MURENZI <56021941+dean-daryl@users.noreply.github.com>
2023-05-07 13:51:53 +0200

    ch()-react-router-185106764

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+import Footer from './Footer';
+import NavBar from './NavBar';
+import Layout from './Layout';
+
+export { Footer, NavBar, Layout };
---
+++ src/views/Login.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Login = () => {
+  return (
+    <>
+      <div>This is the Login Page</div>
+      <div>
+        <Link to="/">Back Home</Link>
+      </div>
+    </>
+  );
+};
+
+export default Login;
