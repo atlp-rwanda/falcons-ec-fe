d3d6dc77c4f338321108c4d9251c5f2ddbcb22ec
Dean Daryl MURENZI <56021941+dean-daryl@users.noreply.github.com>
2023-05-07 13:54:54 +0200

    ch()-react-router-185106764

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 import Footer from './Footer';
 import NavBar from './NavBar';
 import Layout from './Layout';
+import Counter from './Counter'
 
-export { Footer, NavBar, Layout };
+export { Footer, NavBar, Layout,Counter };
--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 const Login = () => {
   return (
     <>
       <div>This is the Login Page</div>
       <div>
-        <Link to="/">Back Home</Link>
+        <NavLink to="/">Back Home</NavLink>
       </div>
     </>
   );
